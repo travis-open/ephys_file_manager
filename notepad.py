b62a6660d4559f9bc10c663da7757510c319e8e7
Travis Hage <travishage@gmail.com>
2024-02-20 14:24:58 -0800

    update notepad to get active directory

--- notepad.py
+++ notepad.py
@@ -8,7 +8,7 @@ class NotepadGUI(object):
 		self.parent = parent
 		self.root = root
 		self.root.title("notepad")
-		
+		self.save_dir = self.parent.dm.active_directory
 		self.text_box = Text(self.root, height = 20, width = 30)
 		self.text_box.pack(fill=BOTH, expand=True)
 		#text ="""user notes     """
@@ -24,13 +24,15 @@ class NotepadGUI(object):
 		self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
 	
 	def save_text_box(self):
+		directory = self.save_dir
 		notes=self.text_box.get("1.0", "end-1c")
-		with open('notes.txt', 'w') as f:
+		with open(directory/'notes.txt', 'w') as f:
 			f.write(notes)
 
 	def find_notes_file(self):
 		try:
-			notes_file=open("notes.txt","r")
+			directory = self.save_dir
+			notes_file=open(directory/"notes.txt","r")
 			notes_text = notes_file.read()
 			notes_file.close()
 			return notes_text
